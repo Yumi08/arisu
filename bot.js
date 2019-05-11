da63ed3c1d64f954bea664772fad0089472413ff
Alan Lewis <thesorton@gmail.com>
2019-05-11 12:09:37 -0700

    add plugin/music.js to replace bot.js's code

--- bot.js
+++ bot.js
@@ -1,27 +1,21 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
-const { Client, RichEmbed } = require('discord.js');
+const {RichEmbed } = require('discord.js');
 var fs = require("fs");
+
+/* Category-based "extensions" */
+/* Music commands */
+eval(fs.readFileSync('plugin/music.js')+'');
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setActivity('%help')  
   });
   
   client.on('message', msg => {
     if (msg.content === `${config.prefix}ping`) {
 		msg.channel.send('Pong.');
-    }
-  });
-  
-  client.on('message', msg => {
-    if (msg.content === `${config.prefix}radio`) {
-      const voiceChannel = msg.member.voiceChannel;
-      voiceChannel.join()
-      .then(connection => {
-        return connection.playStream('http://lainon.life:8000/cyberia.ogg');
-       })
-
     }
   });
 
@@ -30,7 +24,7 @@ client.on('ready', () => {
       const embed = new RichEmbed()
         .setTitle('Help')
         .setColor(0xFF0000)
-        .setDescription('%radio -- Plays lainchan radio');
+        .setDescription('%radio -- Plays lainchan radio \n %leave -- Leaves voice channel \n More things will be added along the way so stay tuned! 😅');
       message.channel.send(embed);
     }
   });
---
+++ plugin/music.js
@@ -0,0 +1,16 @@
+  client.on('message', msg => {
+    if (msg.content === `${config.prefix}radio`) {
+      const voiceChannel = msg.member.voiceChannel;
+      voiceChannel.join()
+      .then(connection => {
+        return connection.playStream('http://lainon.life:8000/cyberia.ogg');
+       })
+    }
+  });
+
+  client.on('message', msg => {
+    if (msg.content === `${config.prefix}leave`) {
+      const voiceChannel = msg.member.voiceChannel;
+      voiceChannel.leave()
+    }
+  });
