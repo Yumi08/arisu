488bb7190798ca1012b37bb944dd30c4e77edc77
Alan Lewis <thesorton@gmail.com>
2019-05-12 09:31:35 -0700

    read prefix from config.json

--- plugin/help.js
+++ plugin/help.js
@@ -1,32 +1,32 @@
 /* Category overview */
-client.on('message', message => {
+client.on(`message`, message => {
     if (message.content === `${config.prefix}help`) {
       const embed = new RichEmbed()
-        .setTitle('Help')
+        .setTitle(`Help`)
         .setColor(0xFF0000)
-        .setDescription('**%help sounds** -- Sound commands\n **%help utils** -- Utilities\n**More things will be added along the way so stay tuned!**');
+        .setDescription(`**${config.prefix}help sounds** -- Sound commands\n **${config.prefix}help utils** -- Utilities\n**More things will be added along the way so stay tuned!**`);
       message.channel.send(embed);
     }
   });
 
 /* Sound commands */
-client.on('message', message => {
+client.on(`message`, message => {
     if (message.content === `${config.prefix}help sounds`) {
       const embed = new RichEmbed()
-        .setTitle('Sounds')
+        .setTitle(`Sounds`)
         .setColor(0xFF0000)
-        .setDescription('**%radio** -- Plays lainchan radio \n**%leave** -- Leaves voice channel\n**%powerlines** -- Plays 3 hours of powerlines');
+        .setDescription(`**${config.prefix}radio** -- Plays lainchan radio \n**${config.prefix}leave** -- Leaves voice channel\n**${config.prefix}powerlines** -- Plays 3 hours of powerlines`);
       message.channel.send(embed);
     }
   });
 
 /* Utility commands */
-client.on('message', message => {
+client.on(`message`, message => {
     if (message.content === `${config.prefix}help utils`) {
       const embed = new RichEmbed()
-        .setTitle('Utilities')
+        .setTitle(`Utilities`)
         .setColor(0xFF0000)
-        .setDescription('**%code** -- Source code \n**%ping** -- Pong');
+        .setDescription(`**${config.prefix}code** -- Source code \n**${config.prefix}ping** -- Pong`);
       message.channel.send(embed);
     }
   });
