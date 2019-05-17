81a6a7b60d5ff90d8f3bace1c2fb7c8147f14adf
Alan Lewis <thesorton@gmail.com>
2019-05-17 08:06:52 -0700

    change help embed

--- plugin/help.js
+++ plugin/help.js
@@ -1,50 +1,28 @@
-/* Category overview */
+/* Help */
 client.on(`message`, message => {
     if (message.content === `${config.prefix}help`) {
-      const embed = new RichEmbed()
-        .setTitle(`Help`)
-        .setColor(0xFF0000)
-        .setDescription(`**${config.prefix}help sounds** -- Sound commands
-        **${config.prefix}help utils** -- Utilities
-        **${config.prefix}help mod** -- Moderation
-        **More things will be added along the way so stay tuned!**`);
-      message.channel.send(embed);
-    }
-  });
-
-/* Sound commands */
-client.on(`message`, message => {
-    if (message.content === `${config.prefix}help sounds`) {
-      const embed = new RichEmbed()
-        .setTitle(`Sounds`)
-        .setColor(0xFF0000)
-        .setDescription(`**${config.prefix}radio** -- Plays lainchan radio
-        **${config.prefix}leave** -- Leaves voice channel`);
-      message.channel.send(embed);
-    }
-  });
-
-/* Utility commands */
-client.on(`message`, message => {
-    if (message.content === `${config.prefix}help utils`) {
-      const embed = new RichEmbed()
-        .setTitle(`Utilities`)
-        .setColor(0xFF0000)
-        .setDescription(`**${config.prefix}code** -- Source code
-        **${config.prefix}ping** -- Pong`);
-      message.channel.send(embed);
-    }
-  });
-
-  client.on(`message`, message => {
-    if (message.content === `${config.prefix}help mod`) {
-      const embed = new RichEmbed()
-        .setTitle(`Moderation`)
-        .setColor(0xFF0000)
-        .setDescription(`**${config.prefix}kick** -- Kicks a user
-        **${config.prefix}ban** -- Bans a user
-        **${config.prefix}mute** -- Mutes a user
-        **You must have the "overlords" role for these commands to do anything.**`);
-      message.channel.send(embed);
+        message.channel.send({embed: {
+        color: 15158332,
+        author: {
+          name: client.user.username,
+          icon_url: client.user.avatarURL
+        },
+        title: "Help commands",
+        description: `Here are the various commands. The prefix is **${config.prefix}** for each of the commands`,
+        fields: [{
+            name: "Sounds",
+            value: "**radio** -- This plays lainchain radio. \n**leave** -- This leaves the voice channel."
+          },
+          {
+            name: "Utilities",
+            value: "**ping** -- Pings the bot and returns a Pong. \n**code** -- This links the source code to the bot."
+          },
+          {
+            name: "Moderation",
+            value: "**kick** -- This kicks the requested user. \n**ban** -- This bans the requested user. \n**mute** -- This gives the requested user the muted role."
+          }
+        ],
+      }
+    });
     }
   });
