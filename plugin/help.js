41a9431fd9ca753721c531d66cf9057b2063aafa
Alan Lewis <thesorton@gmail.com>
2019-05-17 08:24:27 -0700

    chnage embed color

--- plugin/help.js
+++ plugin/help.js
@@ -2,24 +2,24 @@
 client.on(`message`, message => {
     if (message.content === `${config.prefix}help`) {
         message.channel.send({embed: {
-        color: 15158332,
+        color: 3066993,
         author: {
           name: client.user.username,
           icon_url: client.user.avatarURL
         },
         title: "Help",
-        description: `Here are the various commands. The prefix is **${config.prefix}** for each of the commands`,
+        description: "Here are the various commands.",
         fields: [{
             name: "Sounds",
-            value: "**radio** -- This plays lainchain radio. \n**leave** -- This leaves the voice channel."
+            value: `**${config.prefix}radio** -- This plays lainchain radio. \n**${config.prefix}leave** -- This leaves the voice channel.`
           },
           {
             name: "Utilities",
-            value: "**ping** -- Pings the bot and returns a Pong. \n**code** -- This links the source code to the bot."
+            value: `**${config.prefix}ping** -- Pings the bot and returns a Pong. \n**${config.prefix}code** -- This links the source code to the bot.`
           },
           {
             name: "Moderation",
-            value: "**kick** -- This kicks the requested user. \n**ban** -- This bans the requested user. \n**mute** -- This gives the requested user the muted role.\n**Note you need the mod role to use these commands.**"
+            value: `**${config.prefix}kick** -- This kicks the requested user. \n**${config.prefix}ban** -- This bans the requested user. \n**${config.prefix}mute** -- This gives the requested user the muted role.\n**Note you need the mod role to use these commands.**`
           }
         ],
       }
