44b5bade6c67fda6a06b29f198086936b32cb766
Alan Lewis <thesorton@gmail.com>
2019-05-30 16:55:35 -0700

    code shouldn't be an embed

--- plugin/util.js
+++ plugin/util.js
@@ -2,22 +2,18 @@
 
 client.on('message', message => {
     if (message.content === `${config.prefix}ping`) {
-      message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");  
+      message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");
      }
   });
 
   client.on('message', message => {
     if (message.content === `${config.prefix}code`) {
-      const embed = new RichEmbed()
-        .setTitle('Source Code')
-        .setColor(0xFF0000)
-        .setDescription('You can find the source code here \nhttps://github.com/TheSorton/Arisubot');
-      message.channel.send(embed);
+      message.channel.send("https://github.com/TheSorton/Arisubot");
     }
   });
 
   /* hi */
-  
+
   client.on("message", message => {
   if (message.content === 'hi') {
     if (message.author.id !== '146873762367668225') return; {
