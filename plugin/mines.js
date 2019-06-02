54970f06169eac906288f357c81464bac0bfb794
Fungi#2606 @FungiOfDeath <2402020+FungiOfDeath@users.noreply.github.com>
2019-06-02 18:47:09 -0500

    Convert message plugins to use `addCommand`

--- plugin/mines.js
+++ plugin/mines.js
@@ -1,9 +1,3 @@
-client.on("message", async message => {
-  if(message.author.bot) return;
-  if(message.content.indexOf(config.prefix) !== 0) return;
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-
-if (command === "mines") {
+addCommand("mines", ({ message }) => {
   message.reply(`**This command has been removed due to serious lag in the bot, and the channel.**`)
-}});
+});
