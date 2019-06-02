b7756589aecb94682709a801df97d85b7b749f7a
Alan Miles <TheSorton@gmail.com>
2019-06-02 09:24:43 -0700

    rewrite 6/6

--- plugin/util.js
+++ plugin/util.js
@@ -1,42 +1,33 @@
 // Utils
-
-client.on('message', async message => {
-    if (message.content === `${config.prefix}ping`) {
-    message.channel.send(`Latency is ${message.createdTimestamp - message.createdTimestamp}ms.\nAPI Latency is ${Math.round(client.ping)}ms.`);     }
-  });
-
-  client.on('message', async message => {
-    if (message.content === `${config.prefix}code`) {
-      message.channel.send("https://github.com/TheSorton/Arisubot");
-    }
-  });
-
 client.on("message", async message => {
   if(message.author.bot) return;
   if(message.content.indexOf(config.prefix) !== 0) return;
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
-  if(command === "say") {
-  message.channel.send(args)
-}});
 
-client.on("message", async message => {
-  if(message.author.bot) return;
-  if(message.content.indexOf(config.prefix) !==0) return;
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-  if(command === "avatar") {
-    if (message.content === `${config.prefix}avatar <@530107695630647296>`) {
-      message.channel.send("Please use `lain avatar`")
+if (command === "ping") {
+    message.channel.send(`Latency is ${message.createdTimestamp - message.createdTimestamp}ms.\nAPI Latency is ${Math.round(client.ping)}ms.`);
+};
+
+if (command === "code") {
+    message.channel.send("https://github.com/TheSorton/Arisubot");
+};
+
+if (command === "say") {
+   message.channel.send(args)
+};
+
+if (command === "avatar") {
+  if (message.content === `${config.prefix}avatar <@530107695630647296>`) {
+    message.channel.send("Please use `lain avatar`")
 }
-      else {
-       const user = message.mentions.users.first() || message.author;
-       const avatarEmbed = new Discord.RichEmbed()
-           .setColor(0xbe132d)
-           .setAuthor(`Avatar of ${user.username}`)
-           .setImage(user.avatarURL);
-       message.channel.send(avatarEmbed);
-     }
-   }
+else {
+  const user = message.mentions.users.first() || message.author;
+  const avatarEmbed = new Discord.RichEmbed()
+    .setColor(0xbe132d)
+    .setAuthor(`Avatar of ${user.username}`)
+    .setImage(user.avatarURL);
+  message.channel.send(avatarEmbed);
   }
-);
+};
+});
