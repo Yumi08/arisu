580c0718fa20bcdb6e51892a5d5016d58a8d96aa
Alan Miles <thesorton@gmail.com>
2019-07-05 09:05:22 -0700

    help update

--- plugin/help.js
+++ plugin/help.js
@@ -22,6 +22,9 @@ addCommand("help", ({ message }) => {
         value: `**${config.prefix}ping** -- Pings the bot. \n**${config.prefix}code** -- This links the source code to the bot.\n**${config.prefix}say** -- Says what you want\n**${config.prefix}avatar** -- Gives avatar of mentioned user or your own.`
       },
       {
+        name: "Colors"
+        value: `${config.prefix}list -- List roles.`
+      {
         name: "Moderation",
         value: `**${config.prefix}kick** -- This kicks the requested user. \n**${config.prefix}ban** -- This bans the requested user. \n**${config.prefix}mute** -- This gives the requested user the muted role.\n**${config.prefix}purge** -- Bulk deletes messages. Must be a number between 2 and 100.\n**${config.prefix}warn** -- Warns user.\n**Note you need the mod role to use these commands.**`
       }
